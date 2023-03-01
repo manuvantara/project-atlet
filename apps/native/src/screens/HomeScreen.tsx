7da4ce2c34cf279e46ef0b4b62e8b90c8f84be39
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-03-01 12:12:52 +0100

    style: home & settings screens background color

--- apps/native/src/screens/HomeScreen.tsx
+++ apps/native/src/screens/HomeScreen.tsx
@@ -1,15 +1,14 @@
 import { Button, Center, Heading } from 'native-base';
-import { usePrivateKeyStore } from '../stores/privateKey.store';
+
 import type { ScreenProps } from '../types/navigation';
 
 export default function HomeScreen({ navigation }: ScreenProps<'HomeScreen'>) {
-
   const startSession = () => {
     navigation.navigate('TasksScreen');
   };
 
   return (
-    <Center flex={1} px={4}>
+    <Center flex={1} px={4} className='bg-primary'>
       <Heading size='2xl' className='mb-4'>
         Welcome!
       </Heading>
--- apps/native/src/screens/SettingsScreen.tsx
+++ apps/native/src/screens/SettingsScreen.tsx
@@ -1,11 +1,12 @@
 import { Button, Center, Heading } from 'native-base';
+
 import { usePrivateKeyStore } from '../stores/privateKey.store';
 
 export default function SettingsScreen() {
   const signOut = usePrivateKeyStore((state) => state.signOut);
 
   return (
-    <Center flex={1} px={4}>
+    <Center flex={1} px={4} className='bg-primary'>
       <Heading size='2xl' className='mb-4'>
         Settings
       </Heading>
