1c8d11b77a07a35d2876bfe239f1234179292c08
Vlad Chuchupak <v.chuchupak@gmail.com>
2023-03-01 12:16:46 +0100

    feat[summary-screen]: prevent navigation back, layout update

--- apps/native/src/screens/SummaryScreen.tsx
+++ apps/native/src/screens/SummaryScreen.tsx
@@ -1,13 +1,17 @@
 import { Button, Heading, Text, VStack } from 'native-base';
 import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import type { RootStackScreenProps } from '../types/navigation';
 import { shareFile } from '../utils/SessionsLocalService';
+import { usePreventNavigationBack } from '../utils/hooks/usePreventNavigationBack';
 
 export default function SummaryScreen({
   navigation,
   route,
 }: RootStackScreenProps<'SummaryScreen'>) {
+  usePreventNavigationBack(navigation);
+
   const {
     totalTimeSpent,
     totalSensorReadings,
@@ -24,27 +28,37 @@ export default function SummaryScreen({
   };
 
   return (
-    <View className='flex-1 items-center justify-center h-full'>
-      <Heading size='2xl' className='mb-4'>
-        Thank you very much for your contribution!
-      </Heading>
-      <Text size='lg' className='mb-4'>
-        You have spent {totalTimeSpent.toFixed(3)} seconds and recorded{' '}
-        {totalSensorReadings} sensor readings.
-      </Text>
-      <VStack space={4} alignItems='center' flex={1} justifyContent='center'>
-        <Heading size='md'>{sessionFileUri.split('/')[8]}</Heading>
-        <Button onPress={() => shareSessionSummary(sessionFileUri)}>
-          Share session file
-        </Button>
+    <SafeAreaView className='flex-1 px-4 pb-10 bg-primary'>
+      <View className='pt-36'>
+        <Heading size='2xl' className='mb-4 text-center'>
+          Thank you very much for your contribution!
+        </Heading>
+        <Text className='mb-4 text-center'>
+          You have spent {totalTimeSpent.toFixed(0)} seconds and recorded{' '}
+          {totalSensorReadings} sensor readings.
+        </Text>
+      </View>
+      <VStack className='mb-4' space={6}>
+        <View>
+          <Text className='text-md mb-2 leading-tight tracking-tighter'>
+            {sessionFileUri.split('/')[8]}
+          </Text>
+          <Button onPress={() => shareSessionSummary(sessionFileUri)}>
+            Share session.json
+          </Button>
+        </View>
+        <View>
+          <Text className='text-md mb-2 leading-tight tracking-tighter'>
+            {sensorReadingsFileUri.split('/')[8]}
+          </Text>
+          <Button onPress={() => shareSessionSummary(sensorReadingsFileUri)}>
+            Share sensor-readings.csv
+          </Button>
+        </View>
       </VStack>
-      <VStack space={4} alignItems='center' flex={1} justifyContent='center'>
-        <Heading size='md'>{sensorReadingsFileUri.split('/')[8]}</Heading>
-        <Button onPress={() => shareSessionSummary(sensorReadingsFileUri)}>
-          Share session file
-        </Button>
-      </VStack>
-      <Button onPress={redirectToSessionsHistoryScreen}>Continue</Button>
-    </View>
+      <Button className='mt-auto' onPress={redirectToSessionsHistoryScreen}>
+        Continue
+      </Button>
+    </SafeAreaView>
   );
 }
